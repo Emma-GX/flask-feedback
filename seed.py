22d5d2784cab2ab46bdb8ba59c1a8bb765479764
EmmaGX <emmagomezxolo@gmail.com>
2022-03-18 01:06:12 -0400

    Trying to get seed file to work

--- seed.py
+++ seed.py
@@ -1,6 +1,16 @@
-from models import db
+from models import db, User, Feedback
 from app import app
 
 # Create all tables
-# db.drop_all()
+db.drop_all()
 db.create_all()
+
+# Add user
+
+u = User(
+    username="BambisMom",
+    password="BambisMom",
+    email="emmagomezxolo@gmail.com"
+    first_name="Emma",
+    last_name="Gomez Xolo"
+)
